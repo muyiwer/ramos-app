800a3bca6918c5b7fe7346454242444e4c1ed893
Olumuyiwa Aro <olumuyiwa.aro@xpresspayments.com>
2024-03-24 09:49:18 +0100

    fixed counter

---
+++ src/hooks/useAniminatedValue.ts
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+
+export const useAnimatedValue = (
+  initialValue: number,
+  targetValue: number,
+  duration: number,
+  inView: any
+): number => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [value, setValue] = useState<number>(initialValue);
+console.log(isVisible)
+  useEffect(() => {
+    if (inView) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  }, [inView]);
+
+  useEffect(() => {
+    if (isVisible) {
+      const startTime = Date.now();
+
+      const updateValue = () => {
+        const elapsedTime = Date.now() - startTime;
+        if (elapsedTime >= duration) {
+          setValue(targetValue);
+        } else {
+          const delta = (targetValue - initialValue) * (elapsedTime / duration);
+          setValue(initialValue + delta);
+          requestAnimationFrame(updateValue);
+        }
+      };
+
+      updateValue();
+
+      return () => {
+        // Cleanup
+      };
+    }
+  }, [initialValue, targetValue, duration, isVisible]);
+
+  return value;
+};
